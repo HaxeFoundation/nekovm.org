820bb500c6cb684477a86da289ed78f3d39aa89f
Jason O'Neil <jason.oneil@gmail.com>
2015-07-27 20:26:39 +1000

    Get pages loading as markdown, with table of contents, and with code highlighting.
    Yeehah.

--- src/NekoSite.hx
+++ src/NekoSite.hx
@@ -1,7 +1,13 @@
 import ufront.MVC;
+import sys.FileSystem;
+import sys.io.File;
+using Detox;
 
 @viewFolder("/")
 class NekoSite extends Controller {
+
+	// Site initialisation
+
 	static var ufApp:UfrontApplication;
 
 	static function main() {
@@ -13,11 +19,47 @@ class NekoSite extends Controller {
 		ufApp.executeRequest();
 	}
 
-	@:route("/")
-	public function home() {
-		return new ViewResult({
-			title: "[NekoVM]",
-			currentYear: Date.now().getFullYear()
-		});
+	// Main controller
+
+	@inject public var nekoApi:AsyncNekoApi;
+
+	@:route("/doc/view/$name")
+	public function api( name:String ) {
+		return 'Showing API documentation for $name';
+	}
+
+	@:route("/*")
+	public function page( rest:Array<String> ) {
+		var pageName = rest.join("/");
+		return nekoApi.getPage( pageName ) >> function( page:Page ):ViewResult {
+			var view = new ViewResult( page );
+			view.setVar( "currentYear", Date.now().getFullYear() );
+			return view;
+		};
 	}
 }
+
+typedef Page = {
+	title:String,
+	content:String,
+	toc:String
+}
+
+class NekoApi extends UFApi {
+	@inject("scriptDirectory") public var scriptDir:String;
+
+	public function getPage( fileName:String ):Page {
+		var file = scriptDir + "pages/" + fileName + ".md";
+		if ( FileSystem.exists(file) ) {
+			var md = File.getContent( file );
+			var data = PageProcessor.processMarkdown( md );
+			return {
+				title: (data.title!=null) ? data.title : fileName,
+				content: data.content.html(),
+				toc: data.toc.html()
+			}
+		}
+		else return throw HttpError.pageNotFound();
+	}
+}
+class AsyncNekoApi extends UFAsyncApi<NekoApi> {}
---
+++ src/PageProcessor.hx
@@ -0,0 +1,69 @@
+using Detox;
+using StringTools;
+
+class PageProcessor {
+	public static function processMarkdown( markdown:String ) {
+		var html = Markdown.markdownToHtml( markdown );
+		var dom = html.parse();
+		return new PageProcessor( dom ).process();
+	}
+
+	var dom:DOMCollection = null;
+	var mainTOC:DOMCollection = null;
+	var currentIndentLevel:Int = 1;
+	var currentMenu:DOMCollection = null;
+	var currentLink:DOMCollection = null;
+
+	public function new ( dom ) {
+		this.dom = dom;
+	}
+
+	function process():{ title:String, content:DOMCollection, toc:DOMCollection } {
+		var title:String = null;
+		mainTOC = newTableOfContents();
+		currentMenu = mainTOC;
+
+		for ( child in dom ) if ( child.nodeType==Xml.Element ) {
+			switch child.nodeName {
+				case "h1":
+					addLink( child, 1 );
+					if ( title==null )
+						title = child.text();
+				case "h2": addLink( child, 2 );
+				case "h3": addLink( child, 3 );
+				case "h4": addLink( child, 4 );
+			}
+		}
+		return {
+			title: title,
+			content: dom,
+			toc: mainTOC
+		}
+	}
+
+	function addLink( headerNode:DOMNode, indent:Int ) {
+		var title = headerNode.text();
+
+		// Figure out a href for this header.
+		var url = title.trim().toLowerCase().replace( " ", "_" );
+		url = ~/[^a-z0-9_]/g.replace( url, "" );
+		headerNode.setAttr( "id", url );
+
+		this.currentMenu =
+			if ( indent>currentIndentLevel ) newTableOfContents( currentLink )
+			else if ( indent<currentIndentLevel ) currentMenu.parent().parent()
+			else currentMenu;
+
+		this.currentLink = appendLinkToTOC( currentMenu, indent, url, title );
+		this.currentIndentLevel = indent;
+	}
+
+	function newTableOfContents( ?parentLink:DOMCollection ):DOMCollection {
+		return '<ul class="toc"></ul>'.parse().appendTo( parentLink );
+	}
+
+	function appendLinkToTOC( toc:DOMCollection, level:Int, link:String, title:String ):DOMCollection {
+		var linkHTML = '<li class="level${level}"><span class="li"><a href="#$link">$title</a></span></li>';
+		return linkHTML.parse().appendTo( toc );
+	}
+}
