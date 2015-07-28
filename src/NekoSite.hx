ef91d882c3d61ca09b38b2470f34b8def1670ae1
Jason O'Neil <jason.oneil@gmail.com>
2015-07-28 11:55:09 +1000

    Add sitemap.
    Also moved NekoApi out into it's own file now that it's a little bigger.

--- src/NekoSite.hx
+++ src/NekoSite.hx
@@ -1,7 +1,5 @@
 import ufront.MVC;
-import sys.FileSystem;
-import sys.io.File;
-using Detox;
+import NekoApi;
 
 @viewFolder("/")
 class NekoSite extends Controller {
@@ -22,6 +20,34 @@ class NekoSite extends Controller {
 	// Main controller
 
 	@inject public var nekoApi:AsyncNekoApi;
+
+	@:route("/sitemap/")
+	public function sitemap() {
+		return nekoApi.getSitemap() >> function( sitemap:Sitemap ) {
+			var ul = sitemapToList( sitemap );
+			return new ViewResult({
+				title: "Sitemap",
+				content: '<h1>Sitemap</h1>'+ul,
+				currentYear: Date.now().getFullYear(),
+				editLink: 'https://github.com/HaxeFoundation/nekovm.org/blob/master/www/pages/',
+			}, "page.html");
+		}
+	}
+
+	function sitemapToList( sitemap:Sitemap ):String {
+		var buf = new StringBuf();
+		buf.add( '<ul class="sitemap">' );
+		for ( link in sitemap ) {
+			buf.add( '<li>' );
+			var label = (link.link!=null) ? '<a href="${link.link}">${link.name}</a>' : '${link.name}';
+			buf.add( label );
+			if ( link.children!=null ) {
+				buf.add( sitemapToList(link.children) );
+			}
+		}
+		buf.add( '</ul>' );
+		return buf.toString();
+	}
 
 	@:route("/doc/view/$name")
 	public function api( name:String ) {
@@ -44,44 +70,3 @@ class NekoSite extends Controller {
 		};
 	}
 }
-
-typedef Page = {
-	title:String,
-	content:String,
-	toc:String
-}
-
-class NekoApi extends UFApi {
-	@inject("scriptDirectory") public var scriptDir:String;
-
-	/** Load a markdown page, convert to HTML, extract the document title and table of contents. **/
-	public function getPage( fileName:String ):Page {
-		var file = scriptDir + "pages/" + fileName + ".md";
-		if ( FileSystem.exists(file) ) {
-			var md = File.getContent( file );
-			var data = PageProcessor.processMarkdown( md );
-			return {
-				title: (data.title!=null) ? data.title : fileName,
-				content: data.content.html(),
-				toc: data.toc.html()
-			}
-		}
-		else return throw HttpError.pageNotFound();
-	}
-
-	/** Load some API XML, convert to HTML, extract the document title, description, and build table of contents. **/
-	public function getApi( fileName:String ):Page {
-		var file = scriptDir + "api/" + fileName + ".xml";
-		if ( FileSystem.exists(file) ) {
-			var xml = File.getContent( file );
-			var data = PageProcessor.processHtml( xml );
-			return {
-				title: (data.title!=null) ? data.title : fileName,
-				content: data.content.html(),
-				toc: data.toc.html()
-			}
-		}
-		else return throw HttpError.pageNotFound();
-	}
-}
-class AsyncNekoApi extends UFAsyncApi<NekoApi> {}
