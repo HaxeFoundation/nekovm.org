1e84200c456f515179566bc86adee54a8d87fd92
Tobiasz Laskowski <tobil4sk@outlook.com>
2024-07-18 16:26:15 +0100

    Remove old deploy script

--- src/Config.hx
+++ src/Config.hx
@@ -18,11 +18,7 @@ class Config {
 		}
 	};
 
-	static public var remote   = env("GHP_REMOTE",   null); // should be in the form of https://token@github.com/account/repo.git
-	static public var branch   = env("GHP_BRANCH",   "gh-pages");
-	static public var username = env("GHP_USERNAME", null);
-	static public var email    = env("GHP_EMAIL",    null);
-	static public var root     = env("GHP_ROOT",     null);
+	static public var root = env("GHP_ROOT", null);
 
 	static public function env(name:String, def:String):String {
 		return switch(Sys.getEnv(name)) {
